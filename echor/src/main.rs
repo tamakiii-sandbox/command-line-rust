71e5e49064ec51c7ca68c35089f75df9ec0a858c
tamakiii <tamakiii@users.noreply.github.com>
2025-01-03 21:10:09 +0900

    Command::new()

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,9 @@
+use clap::Command;
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let _matches = Command::new("echor")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust echo")
+        .get_matches();
 }

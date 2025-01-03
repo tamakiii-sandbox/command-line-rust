157659b177aaa77057e77dcaf2e7219d3f06180d
tamakiii <tamakiii@users.noreply.github.com>
2025-01-03 21:14:53 +0900

    add args

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,9 +1,22 @@
-use clap::Command;
+use clap::{Arg, Command};
 
 fn main() {
     let _matches = Command::new("echor")
         .version("0.1.0")
         .author("Ken Youens-Clark <kyclark@gmail.com>")
         .about("Rust echo")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do no print newline")
+                .num_args(0),
+        )
         .get_matches();
 }

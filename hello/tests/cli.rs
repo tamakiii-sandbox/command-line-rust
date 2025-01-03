8e4b171244ba1fa348f0f22118ba4f1881baaa11
tamakiii <tamakiii@users.noreply.github.com>
2025-01-03 17:38:16 +0900

    add test true_ok

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -1,7 +1,13 @@
 use assert_cmd::Command;
 
 #[test]
-fn works() {
+fn runs() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
     cmd.assert().success();
 }

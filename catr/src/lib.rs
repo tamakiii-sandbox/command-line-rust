d8e91a0bc13973d01a38ae4803b10d37b9e365ac
tamakiii <tamakiii@users.noreply.github.com>
2025-01-04 13:34:11 +0900

    impl

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,9 +1,11 @@
 use clap::{Arg, Command};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Lines};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Config {
     files: Vec<String>,
     number_lines: bool,
@@ -11,7 +13,30 @@ pub struct Config {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(error) => eprintln!("Failed to open {}: {}", filename, error),
+            Ok(buffer) => {
+                let mut last_num = 0;
+                for (index, row) in buffer.lines().enumerate() {
+                    let line = row?;
+
+                    if config.number_lines {
+                        println!("{:>6}\t{}", index + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:>6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
 
@@ -55,4 +80,11 @@ pub fn get_args() -> MyResult<Config> {
         number_lines: matches.get_flag("number"),
         number_nonblank_lines: matches.get_flag("number_nonblank"),
     })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }

56e3ee9fe68416ab22e290f6b95855c716bcb877
tamakiii <tamakiii@users.noreply.github.com>
2025-01-04 11:29:09 +0900

    temporary implemented get_args

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,8 +1,30 @@
+use clap::Command;
 use std::error::Error;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()> {
-    println!("Hello, world!");
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
     Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("catr")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust cat")
+        .get_matches();
+
+    Ok(Config {
+        files: vec![String::from("-")],
+        number_lines: false,
+        number_nonblank_lines: false,
+    })
 }
--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if let Err(error) = catr::run() {
+    if let Err(error) = catr::get_args().and_then(catr::run) {
         eprintln!("{}", error);
         std::process::exit(1);
     }

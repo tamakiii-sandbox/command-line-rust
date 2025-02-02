d1e9942f7ce0435b57c90a116801ef6701e9324b
tamakiii <tamakiii@users.noreply.github.com>
2025-02-02 12:38:59 +0900

    implement count

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{Arg, Command};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -12,8 +14,30 @@ pub struct Config {
     chars: bool,
 }
 
+#[derive(Debug, PartialEq)]
+pub struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
+}
+
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    for filename in &config.files {
+        match open(filename) {
+            Err(error) => eprintln!("{}: {}", filename, error),
+            Ok(file) => match count(file) {
+                Err(error) => eprintln!("{}: {}", filename, error),
+                Ok(info) => {
+                    println!(
+                        "\t{}\t{}\t{}\t{}\t{}",
+                        info.num_lines, info.num_words, info.num_bytes, info.num_chars, filename
+                    );
+                }
+            },
+        }
+    }
+
     Ok(())
 }
 
@@ -87,4 +111,105 @@ pub fn get_args() -> MyResult<Config> {
         bytes,
         chars,
     })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+// pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+//     let mut num_lines = 0;
+//     let mut num_words = 0;
+//     let mut num_bytes = 0;
+//     let mut num_chars = 0;
+//     let mut line = String::new();
+
+//     loop {
+//         let line_bytes = file.read_line(&mut line)?;
+//         if line_bytes == 0 {
+//             break;
+//         }
+//         num_bytes += line_bytes;
+//         num_lines += 1;
+//         num_words += line.split_whitespace().count();
+//         num_chars += line.chars().count();
+//         line.clear();
+//     }
+
+//     Ok(FileInfo {
+//         num_lines,
+//         num_words,
+//         num_bytes,
+//         num_chars,
+//     })
+// }
+
+fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let num_lines = contents.lines().count();
+    let num_words = contents.split_whitespace().count();
+    let num_bytes = contents.len();
+    let num_chars = contents.chars().count();
+
+    Ok(FileInfo {
+        num_lines,
+        num_words,
+        num_bytes,
+        num_chars,
+    })
+}
+
+// fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+//     let mut num_lines = 0;
+//     let mut num_words = 0;
+//     let mut num_bytes = 0;
+//     let mut num_chars = 0;
+
+//     let mut line = String::new();
+//     let mut reader = file;
+
+//     while {
+//         line.clear();
+//         reader.read_line(&mut line)?
+//     } > 0
+//     {
+//         num_lines += 1;
+//         num_bytes += line.len();
+//         num_words += line.split_whitespace().count();
+//         num_chars += line.chars().count();
+//         line.clear();
+//     }
+
+//     Ok(FileInfo {
+//         num_lines,
+//         num_words,
+//         num_bytes,
+//         num_chars,
+//     })
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::{count, FileInfo};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world. I just want your half.\r\n";
+        let info = count(Cursor::new(text));
+        assert!(info.is_ok());
+        let expected = FileInfo {
+            num_lines: 1,
+            num_words: 10,
+            num_bytes: 48,
+            num_chars: 48,
+        };
+
+        assert_eq!(info.unwrap(), expected);
+    }
 }
